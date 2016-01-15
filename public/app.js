d1afcfc3aee43baaa0bb3a5f6bb6648a320bb892
James Moon <mooniker@gmail.com>
2016-01-15 11:15:42 -0500

    Force message box to scroll to bottom automatically (buggy?)

--- public/app.js
+++ public/app.js
@@ -190,6 +190,21 @@
     };
   });
 
+  app.directive('scrollToBottom', function() {
+    return {
+      scope: {
+        scrollToBottom: '='
+      },
+      link: function(scope, element) {
+        scope.$watchCollection('scrollToBottom', function(newValue) {
+          if (newValue) {
+            $(element).scrollTop($(element)[0].scrollHeight);
+          }
+        });
+      }
+    };
+  });
+
 })();
 
 $.scrolline({
