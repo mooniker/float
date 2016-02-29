ff4ff8b2f8130d9a19e033120d00b8f624786566
James Moon <mooniker@gmail.com>
2016-02-28 23:46:08 -0500

    Revise comments on example_env.js

--- example_env.js
+++ example_env.js
@@ -1,12 +1,14 @@
 // # Here's an example of `env.js`
 //
-// `env.js` is where you can put configuration settings for the server's environment.
+// `env.js` is where you can put configuration settings
+// for your server's environment.
 //
-// If you're fine with a simple local setup, just rename this file `env.js` (by removing `example_`)
+// If you're fine with a simple local setup, just rename
+// this file `env.js` (by removing `example_`).
 //
 // Remember that `env.js` is in .gitignore and will be ignored by git.
 
 module.exports = {
-  PORT: 4000, // or whatever you want
-  MONGO_SERVER_URI: 'mongodb://localhost/float', // or wherever you want to provision your MongoDB
+  PORT: 4000, // or whatever port floats your boat
+  MONGO_SERVER_URI: 'mongodb://localhost/float', // or wherever you provision your MongoDB
 };
